2752b3c3d29f10ba6646eeeb59ee319a04379307
thinkverse <ctuvedsson@gmail.com>
2020-06-09 04:12:27 +0200

    Add back simple navbar component into views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section>
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <h1>This is an about page</h1>
+    </main>
+  </section>
 </template>
 
 <script>
+  import Navbar from '@/components/Navbar.vue'
+
   export default {
+    components: {
+      Navbar
+    },
     metaInfo: {
       title: 'Information about Thinkerse',
     },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <header>
+      <Navbar />
+    </header>
+    <main class="home">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </main>
+    <Footer />
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Navbar from '@/components/Navbar.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld,
+      Navbar
+    }
   }
-}
 </script>

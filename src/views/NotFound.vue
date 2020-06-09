7fce2f71c4bb5cccf63a0c664dcdbdd459e91b08
thinkverse <ctuvedsson@gmail.com>
2020-06-09 03:27:46 +0200

    Update 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="not-found">
-    <h1>Page not found.</h1>
-    <h2>404</h2>
-  </div>
+  <main>
+    <header class="h-screen w-screen bg-gray-100 flex justify-center content-center flex-wrap">
+      <h1 class="font-sans text-gray-800 text-6xl lg:text-7xl">404</h1>
+    </header>
+
+    <footer class="absolute w-screen bottom-0 mb-6 text-black text-center font-sans text-xl">
+      <span class="text-gray-700">Take me back to</span> <a class="border-b" href="https://thinkverse.dev/">thinkverse.dev</a>
+    </footer>
+  </main>
 </template>

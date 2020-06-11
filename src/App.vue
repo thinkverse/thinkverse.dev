f35ae884929297332cd92f9deb38a68f78fab068
Thinkverse <ctuvedsson@gmail.com>
2020-06-11 21:34:41 +0200

    Update meta tags. (#6)
    * Remove hard-coded meta tags.
    * Add boilerplate meta tags.
    * Update meta tags for all existing views.

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,16 @@
     name: 'App',
     metaInfo: {
       title: 'Personal hobby site of Thinkverse',
-      titleTemplate: 'Thinkverse — %s'
+      titleTemplate: 'Thinkverse — %s',
+      meta: [
+        { property: "og:url", content: new URL(window.location).href },
+        { property: "og:type", content: 'website' },
+        { property: 'og:site_name', content: 'Thinkverse'},
+
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: new URL(window.location).href },
+        { name: 'twitter:creator', content: '@Thinkverse' },
+      ],
     }
   }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -16,8 +16,39 @@
     components: {
       Navbar
     },
-    metaInfo: {
-      title: 'Information about Thinkerse',
+    data() {
+      return {
+        meta: {
+          title: 'Information about Thinkerse',
+          description: 'A hobby site where Thinkverse writes about code, books, and everything he finds interesting.',
+        },
+      }
+    },
+    metaInfo()  {
+      const title = `Thinkverse — ${this.$data.meta.title}`;
+
+      const meta = [
+        { name: 'title', content: title },
+        { name: 'description', content: this.$data.meta.description },
+
+        { property: 'og:title', content: title },
+        { property: 'og:description', content: this.$data.meta.description },
+
+        { name: 'twitter:title', content: title },
+        { name: 'twitter:description', content: this.$data.meta.description },
+
+        { itemprop: 'name', content: title },
+        { itemprop: 'description', content: this.$data.meta.description },
+      ];
+
+      if (this.$data.meta.image) {
+        meta.push(...[
+          { name: 'twitter:image:src', content: this.$data.meta.image },
+          { itemprop: 'image', content: this.$data.meta.image },
+        ])
+      }
+
+      return { title: this.$data.meta.title, meta }
     },
   }
 </script>

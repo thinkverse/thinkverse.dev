76e85383fca7ee8c5a7d97a9e32416cfe4aa4935
thinkverse <ctuvedsson@gmail.com>
2020-06-16 21:33:08 +0200

    Update CSS [skip netlify]

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="container">
+  <div class="min-h-screen flex justify-center items-center text-center mx-auto">
     <div>
       <logo />
-      <h1 class="title">
+      <h1 class="text-6xl text-gray-700">
         thinkverse.dev
       </h1>
-      <h2 class="subtitle">
+      <h2 class="text-2xl text-gray-600 font-light">
         My wicked Nuxt.js project
       </h2>
-      <div class="links">
+      <div class="pt-6">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
-          class="button--green"
+          class="border border-green-500 py-3 px-6 mr-4 text-green-500 rounded-md"
         >
           Documentation
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey"
+          class="border border-gray-600 py-3 px-6 text-gray-600 rounded-md"
         >
           GitHub
         </a>
@@ -37,41 +37,3 @@ export default {
   }
 }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>

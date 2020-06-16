9d335c15ab32a27b4a6d1f75b98dee887c5f5211
Thinkverse <ctuvedsson@gmail.com>
2020-06-16 18:23:14 +0200

    Moving over to Nuxt! 🎉 (#7)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.vue',
-  ],
-  theme: {
-    extend: {
-      fontSize: {
-        '7xl': '15rem',
-        'ps5': '11rem',
-      },
-      fontFamily: {
-        'ps5': ["Inter", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
-      }
-    },
-  },
+  theme: {},
   variants: {},
-  plugins: [],
+  plugins: []
 }

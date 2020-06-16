a52ea6753521e36b7a7a3352a5d8f3aa869dd0ff
thinkverse <ctuvedsson@gmail.com>
2020-06-16 21:43:13 +0200

    Update meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,44 @@
-
 export default {
   mode: 'universal',
-  /*
-  ** Headers of the page
-  */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Personal hobby site of Thinkverse',
+    titleTemplate: 'Thinkverse — %s',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'A hobby site where Thinkverse writes about code, books, and everything he finds interesting.' },
+
+      { hid: 'og:title', property: 'og:title', content: 'Personal hobby site of Thinkverse' },
+      { hid: 'og:description', property: 'og:description', content: 'A hobby site where Thinkverse writes about code, books, and everything he finds interesting.' },
+      { hid: 'og:url', property: 'og:url', content: 'https://thinkverse.dev' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Thinkverse' },
+
+      { hid: 'twitter:title', name: 'twitter:title', content: 'Personal hobby site of Thinkverse' },
+      { hid: 'twitter:description', name: 'twitter:description', content: 'A hobby site where Thinkverse writes about code, books, and everything he finds interesting.' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: 'https://thinkverse.dev' },
+      { hid: 'twitter:creator', name: 'twitter:creator', content: '@Thinkverse' },
+
+      { hid: 'ip:name', itemprop: 'name', content: 'Personal hobby site of Thinkverse' },
+      { hid: 'ip:description', itemprop: 'description', content: 'A hobby site where Thinkverse writes about code, books, and everything he finds interesting.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
   loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
   css: [
   ],
-  /*
-  ** Plugins to load before mounting the App
-  */
   plugins: [
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss'
   ],
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
   ],
-  /*
-  ** Build configuration
-  */
   build: {
-    /*
-    ** You can extend webpack config here
-    */
     extend (config, ctx) {
     }
   },

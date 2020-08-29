6508685a18cda42a6b24b510058cd8d1dfa3dba3
thinkverse <hallberg.kim@gmail.com>
2020-08-29 22:15:04 +0200

    Add default fallback view

---
+++ pages/_.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="min-h-screen flex flex-col justify-end items-start md:p-10 lg:p-16">
+    <div class="lg:w-xl">
+      <h1 class="ch-20 text-lg md:text-3xl lg:text-5xl md:leading-1 lg:leading-tight font-bold ml-5 md:m-0 md:mb-3 lg:mb-5">
+        <span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500">
+          Sorry, the page you wanted to see cannot be found.
+        </span>
+      </h1>
+      <NuxtLink to="/" class="text-gray-600 text-sm md:text-base lg:text-lg font-semibold pt-2 pb-5 px-5 md:p-0">
+        Return to home.
+      </NuxtLink>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>

f23a15ae48198c762bb106ee968f8301e2322e45
thinkverse <hallberg.kim@gmail.com>
2020-08-31 20:59:35 +0200

    Add base text and background for prefered color schemes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="antialiased font-sans bg-gray-100 dark:bg-gray-900">
+  <div class="antialiased font-sans bg-gray-100 text-gray-700 dark:bg-gray-900 dark:text-gray-500">
     <nuxt />
   </div>
 </template>

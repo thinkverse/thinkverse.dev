5ebe0079b4aedf62507b019a43add2aaa7420860
thinkverse <hallberg.kim@gmail.com>
2020-08-31 21:05:21 +0200

    Update landing page heading style

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="min-h-screen flex justify-center items-center text-center mx-auto">
     <div>
-      <h1 class="text-6xl text-gray-700">
-        thinkverse.dev
+      <h1 class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-blue-500 dark:from-pink-400  dark:to-indigo-500 ch-20 text-5xl md:text-6xl font-bold">
+        thinkverse
       </h1>
     </div>
   </div>
